824a44eb2274ae5878a7e01c0723a9a30036ee00
Uno <154001298+Uno20@users.noreply.github.com>
2024-05-23 14:29:38 +0800

    Add files via upload

---
+++ src/main/java/com/example/a4and8/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a4and8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, curfew.class);
+                startActivity(intent1);
+
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/a4and8/feedbackSuggestions.java
@@ -0,0 +1,14 @@
+package com.example.a4and8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class feedbackSuggestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback_suggestions);
+    }
+}
